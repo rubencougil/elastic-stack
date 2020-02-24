9c90c3c678db13fa99bee448ecb4dcef96862c92
rubencougil <rubencougil@gmail.com>
2020-02-24 12:11:28 +0100

    randomuser is broken

--- app/user/user.go
+++ app/user/user.go
@@ -2,6 +2,8 @@ package user
 
 import (
 	"encoding/json"
+	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"io/ioutil"
@@ -23,24 +25,38 @@ type randomUser struct {
 	} `json:"results"`
 }
 
-func CreateUserHandler(logger *logrus.Logger, store Store) gin.HandlerFunc {
-	fn := func(c *gin.Context) {
-		u := generate()
+func CreateUserHandler(logger *logrus.Logger, store Store) (fn gin.HandlerFunc) {
+	fn = func(c *gin.Context) {
+		u, err := generate()
+		if err != nil {
+			logger.WithFields(logrus.Fields{"error": err}).Error("Error handling Create User")
+			c.JSON(500, gin.H{"message": "cannot generate a new random user"})
+			return
+		}
 		if err := store.Save(u); err != nil {
 			logger.WithFields(logrus.Fields{"error": err}).Error("Error handling Create User")
-			c.JSON(500, gin.H{"message": "cannot create new user",})
+			c.JSON(500, gin.H{"message": "cannot create new user"})
 			return
 		}
 		logger.WithFields(logrus.Fields{"name": u.Name, "email": u.Email}).Info("New user has been created")
 		c.JSON(200, gin.H{"message": "ok",})
 	}
 
-	return gin.HandlerFunc(fn)
+	return
 }
 
-func generate() *User {
+func generate() (user *User, err error) {
 
-	res, _ := http.Get("https://randomuser.me/api/")
+	res, err := http.Get("https://randomuser.me/api/")
+
+	if err != nil {
+		return
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, errors.New(fmt.Sprintf("Random User is not working: %d", res.StatusCode))
+	}
+
 	defer res.Body.Close()
 	body, _ := ioutil.ReadAll(res.Body)
 	ru := randomUser{}
@@ -49,5 +65,5 @@ func generate() *User {
 	return &User{
 		Name:  ru.Results[0].Name.First + " " + ru.Results[0].Name.Last,
 		Email: ru.Results[0].Email,
-	}
+	}, nil
 }
